fcc29fc6d222ee93984ebb0d07b832aa181301c5
ztreamhd <75388941+ztreamhd@users.noreply.github.com>
2022-01-12 15:06:11 +0000

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -23,7 +23,7 @@ if len(sys.argv) == 2:
                 'https' : sys.argv[1]
               }
 
-na = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u'
+na = 'https://raw.githubusercontent.com/ztreamhd/xtrakt/main/assets/not_available.mp4'
 def grab(line):
     try:
         _id = line.split('/')[4]
