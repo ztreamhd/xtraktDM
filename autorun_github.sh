e8322455a3bba73fec75e0a90350b70fed1214b6
ztreamhd <75388941+ztreamhd@users.noreply.github.com>
2022-04-25 16:46:14 +0100

    Update autorun_github.sh

--- autorun_github.sh
+++ autorun_github.sh
@@ -2,7 +2,7 @@
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
 python3 dailymotion_m3ugrabber.py > ../dailymotion-US.m3u
-python3 dailymotion_m3ugrabber.py "212.129.57.194:41258" > ../dailymotion-EU.m3u
-python3 dailymotion_m3ugrabber.py "104.248.150.190:3128" > ../dailymotion-SG.m3u
+python3 dailymotion_m3ugrabber.py "77.145.26.127" > ../dailymotion-EU.m3u
+python3 dailymotion_m3ugrabber.py "23.51.138.120" > ../dailymotion-SG.m3u
 
 echo m3u grabbed
