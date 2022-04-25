fa3b3501189cb925fa7dda86c28ecb8a3d57f3e1
ztreamhd <75388941+ztreamhd@users.noreply.github.com>
2022-04-25 17:00:30 +0100

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -3,5 +3,7 @@
 echo Creating your playlist...
 cd scripts
 python dailymotion_m3ugrabber.py > ../dailymotion.m3u
+python3 dailymotion_m3ugrabber.py "51.195.76.214:3128" > ../dailymotion-EU.m3u
+python3 dailymotion_m3ugrabber.py "128.199.202.122:3128" > ../dailymotion-SG.m3u
 
 pause
