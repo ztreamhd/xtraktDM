0f30219c56e4807b5049a7ef1c4a2bc3a70c4487
ztreamhd <75388941+ztreamhd@users.noreply.github.com>
2022-04-25 16:45:41 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,4 +2,6 @@
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
 python3 dailymotion_m3ugrabber.py > ../dailymotion.m3u
+python3 dailymotion_m3ugrabber.py "77.145.26.127" > ../dailymotion-EU.m3u
+python3 dailymotion_m3ugrabber.py "23.51.138.120" > ../dailymotion-SG.m3u
 echo m3u grabbed
